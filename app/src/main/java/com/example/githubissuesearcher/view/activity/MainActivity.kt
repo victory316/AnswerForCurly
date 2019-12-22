a0b7e8c83cd2ad36e898bb467ef27eb1f44df3e6
victory316 <csh153@gmail.com>
2019-12-23 00:05:47 +0900

    adding more dependencies, setting up recyclerView

--- app/src/main/java/com/example/githubissuesearcher/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/githubissuesearcher/view/activity/MainActivity.kt
@@ -6,16 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubissuesearcher.R
 import com.example.githubissuesearcher.data.local.entity.GithubData
+import com.example.githubissuesearcher.data.remote.GithubClient
 import com.example.githubissuesearcher.databinding.ActivityMainBinding
 import com.example.githubissuesearcher.view.adapter.GithubSearchAdapter
 import com.example.githubissuesearcher.viewmodel.GithubViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var githubViewModel: GithubViewModel
     private lateinit var githubSearchAdapter: GithubSearchAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
     private var searchDisposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +37,13 @@ class MainActivity : AppCompatActivity() {
 
         setupViewModel()
         setupBinding()
+        setupView()
+    }
+
+    private fun setupView() {
+        linearLayoutManager = LinearLayoutManager(this)
+        binding.searchRecyclerView.layoutManager = linearLayoutManager
+        binding.searchRecyclerView.adapter = githubSearchAdapter
     }
 
     private fun setupBinding() {
@@ -64,8 +77,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun doSearch() {
+        // 키보드 숨
         hideKeyboard()
-        Toast.makeText(this, "search!", Toast.LENGTH_SHORT).show()
+
+        val target = binding.searchEditText.text.toString()
+
+        // Gihub search query로 찾고자 하는 유저를 검색
+        searchDisposable = GithubClient().getApi().searchUser(target)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({ result ->
+//                githubViewModel.insertList(result.name)
+
+            }, {
+                    error ->
+                run {
+                    error.printStackTrace()
+                    Toast.makeText(this, "결과가 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        githubViewModel.deleteAll()
     }
 
 }
